6491e026330c57336eb656445bc45cb1540e4fd1
jianzh5 <jianzh5@163.com>
2019-10-16 23:32:48 +0800

    分库分表

--- blog/src/main/java/com/github/jianzh5/blog/BlogApplication.java
+++ blog/src/main/java/com/github/jianzh5/blog/BlogApplication.java
@@ -3,6 +3,7 @@ package com.github.jianzh5.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//启动类
 @SpringBootApplication
 public class BlogApplication {
 
